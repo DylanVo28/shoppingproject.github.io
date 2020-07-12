14567bc4f91311ae2b83af4bc57cc502a08fe558
DylanVo28 <you@example.com>
2020-07-12 20:12:47 +0700

    ok

---
+++ 1.js
@@ -0,0 +1,37 @@
+﻿ $(function(){
+ 	var vitri=$("header .logo .row .bg-faded li:nth-child(1)").position().left+430-25 +"px";
+ 	var chieudai=$("header .logo .row .bg-faded li:nth-child(1)").width()-18.059 + "px";
+ 	// console.log($("body").width()+"px");
+ 	// console.log(vitri);
+
+ 	$("header .logo .row .bg-faded .themove").css({
+ 			"left": vitri,
+ 			"width": chieudai,
+ 		});
+
+ 	$("header .logo .row .bg-faded li").hover(function() {
+ 		vitri=$(this).position().left+430-25 +"px";
+ 		chieudai=$(this).width() + "px";
+ 		$("header .logo .row .bg-faded .themove").css({
+ 			"left": vitri,
+ 			"width": chieudai,
+ 		});
+ 		$("header .logo .row .bg-faded li a").removeClass('fonttrang');
+ 		$(this).find('a').addClass('fonttrang');
+ 	}, function() {
+ 		$("header .logo .row .bg-faded .themove").css({
+ 			"left": "421px",
+ 			"width": "105px",
+ 		});
+
+ 		$("header .logo .row .bg-faded li a").removeClass('fonttrang');
+ 		$("header .logo .row .bg-faded li:nth-child(1) a").addClass('fonttrang');
+ 	});
+ 	$(window).scroll(function(event) {
+ 		$("header .logo .row .bg-faded .themove").css({
+ 			"top": 171-$(window).scrollTop() +"px",
+ 			
+ 		});
+ 	});
+})  
+ 
