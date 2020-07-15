565be38efbf547ec876a752c989c3d91d18d5ac6
DylanVo28 <you@example.com>
2020-07-15 16:47:40 +0700

    them 1 chut animate va nut chuyen doi ngon ngu

--- 1.js
+++ 1.js
@@ -2,6 +2,42 @@
  	var vitri=$("header .menutop .menu .navbar ul li:nth-child(2)").position().left+"px";
  	var kt=true;
  	var chieurong;
+ 	var ngonngu=false;
+ 	var donam=false;
+ 	var donu=false;
+ 	var dotreem=false;
+ 	gsap.from("section.thanhslide",3,{opacity:0})
+ 	
+ 	$(window).scroll(function(event) {
+		console.log($(window).scrollTop());
+ 		if($(window).scrollTop()>270){
+ 			
+ 			if(donam==false){
+ 				gsap.from("section.quanaonam .text h1",1,{x:300,opacity:0})
+ 				gsap.from("section.quanaonam .text a",1,{x:300,opacity:0})
+ 				gsap.to("section.quanaonam .anh div#carousel-example-generic",1,{opacity:1})
+ 				donam=true;
+ 			}
+			
+					
+ 		}
+ 		if($(window).scrollTop()>970){
+ 			if(donu==false){
+				gsap.from("section.quanaonu .text h1",1,{x:-300,opacity:0})
+ 				gsap.from("section.quanaonu .text a",1,{x:-300,opacity:0})
+ 				gsap.to("section.quanaonu .anh div#carousel-example-generic",1,{opacity:1})
+ 				donu=true;
+			}
+ 		}
+ 		if($(window).scrollTop()>1680){
+ 			if(dotreem==false){
+				gsap.from("section.quanaotreem .text h1",1,{x:300,opacity:0})
+ 				gsap.from("section.quanaotreem .text a",1,{x:300,opacity:0})
+ 				gsap.to("section.quanaotreem .anh div#carousel-example-generic",1,{opacity:1})
+ 				dotreem=true;
+			}
+ 		}
+ 	});
  	$("header .menutop .menu .navbar ul .themove").css({
  			"left": vitri,
  			"width": "117.86px",
@@ -57,12 +93,23 @@
  		}
  		
  	});
- 	$("header .thanhden ul li:nth-child(1) a").click(function(event) {
+ 	$("header .thanhden ul li:nth-child(2) a").click(function(event) {
  		$(".signin").css({
  			"opacity": '1',
  			"visibility": 'visible',
  			"z-index":"30",
  		});
+ 	});
+ 	$("header .thanhden ul li:nth-child(1) a").click(function(event) {
+ 		if(ngonngu==false){
+ 			$(this).html("Tiếng Việt");
+ 			ngonngu=true;
+ 		}
+ 		else{
+ 			$(this).html("English");
+ 			ngonngu=false;
+ 		}
+ 		
  	});
  	$(".signin i").click(function(event) {
  		$(".signin").css({
